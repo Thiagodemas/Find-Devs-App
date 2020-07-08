2f7fa144f392cfb0d94091b116c9afd5649da582
Thiago Demas <thiago.demas7@gmail.com>
2020-07-08 10:58:19 -0300

    create detail page

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { WebView } from 'react-native-webview';
+import styles from './styles';
 
-export default function Profile(){
+export default function Profile({ navigation }){
+    const githubUsername = navigation.getParam('github_username');
+
     return (
-    <View >
-        <Text>Hello Omnistack!</Text>
-    </View>
+        <WebView style={styles.webView} source={{ uri: `https://github.com/${githubUsername}`}} />
     )
 }
