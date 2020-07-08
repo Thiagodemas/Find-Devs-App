90b24bf85cb06848b055212c71a7914dc591c494
Thiago Demas <thiago.demas7@gmail.com>
2020-07-08 10:01:50 -0300

    first commit

---
+++ src/pages/Main/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MapView from 'react-native-maps';
+
+import styles from './styles';
+
+export default function Main(){
+    return (   
+    <MapView style={styles.map}/>
+    )
+}
